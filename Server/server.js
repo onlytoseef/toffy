18aa9bd7da34db7262af2c007f4d525a0dbb5e60
Toseef Rana <hellotoseef@gmail.com>
2024-09-27 22:39:46 +0500

    added home route ins server

--- Server/server.js
+++ Server/server.js
@@ -12,6 +12,9 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use('/URL', urlRoute);
+app.get('/', async (req, res) => {
+  await res.send('<h1>Server is working fine<h1>');
+});
 
 app.get('/:shortUrl', async (req, res) => {
   const shortUrl = req.params.shortUrl;

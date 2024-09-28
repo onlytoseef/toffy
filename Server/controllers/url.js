879c9cfb3291fc05c53ece0cb4d48bb739976295
Toseef Rana <hellotoseef@gmail.com>
2024-09-28 21:24:10 +0500

    changed controller

--- Server/controllers/url.js
+++ Server/controllers/url.js
@@ -1,10 +1,39 @@
+// import ShortUniqueId from 'short-unique-id';
+// import URL from '../models/url.js';
+
+// async function handleCreateShortUrl(req, res) {
+//   try {
+//     const { url } = req.body;
+
+//     if (!url) {
+//       return res.status(400).json({ error: 'URL is required' });
+//     }
+
+//     const uid = new ShortUniqueId({ length: 8 });
+//     const shortUrl = uid();
+
+//     const newUrl = await URL.create({
+//       shortUrl,
+//       redirectUrl: url,
+//       visitHistory: [],
+//     });
+
+//     res.json({ url: shortUrl });
+//   } catch (error) {
+//     console.error('Error in handleCreateShortUrl:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// }
+
+// export default handleCreateShortUrl;
 import ShortUniqueId from 'short-unique-id';
 import URL from '../models/url.js';
 
 async function handleCreateShortUrl(req, res) {
   try {
-    const { url } = req.body;
+    console.log('Received request body:', req.body);
 
+    const { url } = req.body;
     if (!url) {
       return res.status(400).json({ error: 'URL is required' });
     }
@@ -12,12 +41,15 @@ async function handleCreateShortUrl(req, res) {
     const uid = new ShortUniqueId({ length: 8 });
     const shortUrl = uid();
 
+    console.time('dbCreate');
     const newUrl = await URL.create({
       shortUrl,
       redirectUrl: url,
       visitHistory: [],
     });
+    console.timeEnd('dbCreate');
 
+    console.log('Successfully created short URL:', shortUrl);
     res.json({ url: shortUrl });
   } catch (error) {
     console.error('Error in handleCreateShortUrl:', error);
